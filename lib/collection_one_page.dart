bf769a9f6cf98a145cadab3858ceafe54cf63744
Xenios Georgakis <xenios.georgakis@gmail.com>
2025-12-02 22:37:57 +0200

    Add cart item count display to CollectionOnePage and CollectionTwoPage

--- lib/collection_one_page.dart
+++ lib/collection_one_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:union_shop/footer.dart';
+import 'package:union_shop/models/cart_provider.dart';
 import 'package:union_shop/models/product_model.dart';
 
 class CollectionOnePage extends StatefulWidget {
@@ -245,10 +247,46 @@ class _CollectionOnePageState extends State<CollectionOnePage> {
                                     Navigator.pushNamed(context, '/login');
                                   },
                                 ),
-                                IconButton(
-                                  icon: const Icon(Icons.shopping_bag_outlined,
-                                      size: 18, color: Colors.grey),
-                                  onPressed: placeholderCallbackForButtons,
+                                Consumer<CartProvider>(
+                                  builder: (context, cart, child) => Stack(
+                                    children: [
+                                      IconButton(
+                                        icon: const Icon(
+                                          Icons.shopping_bag_outlined,
+                                          size: 18,
+                                          color: Colors.black,
+                                        ),
+                                        onPressed: () {
+                                          Navigator.pushNamed(context, '/cart');
+                                        },
+                                      ),
+                                      if (cart.itemCount > 0)
+                                        Positioned(
+                                          right: 0,
+                                          top: 0,
+                                          child: Container(
+                                            padding: const EdgeInsets.all(2),
+                                            decoration: BoxDecoration(
+                                              color: Colors.red,
+                                              borderRadius:
+                                                  BorderRadius.circular(10),
+                                            ),
+                                            constraints: const BoxConstraints(
+                                              minWidth: 16,
+                                              minHeight: 16,
+                                            ),
+                                            child: Text(
+                                              '${cart.itemCount}',
+                                              style: const TextStyle(
+                                                color: Colors.white,
+                                                fontSize: 10,
+                                              ),
+                                              textAlign: TextAlign.center,
+                                            ),
+                                          ),
+                                        ),
+                                    ],
+                                  ),
                                 ),
                                 IconButton(
                                   icon: const Icon(Icons.menu,
